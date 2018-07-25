2fd99f271928fcf58cf14c2a9831a6f9d14ae96e
lasthelloworld <812691062@qq.com>
2018-07-25 10:44:23 +0800

    1.vsCode 前端使用google调试. 2.flex 弹性布局（一）

---
+++ 前端/flexDemo/js/debuggerTest.js
@@ -0,0 +1,17 @@
+
+
+(function(){
+    
+    console.log("测试调试");
+
+    function test(){
+        var result = 0;
+        for(var i =0;i<6;i++){
+            result+=i;
+        }
+        console.log(result);
+    }
+
+    test();
+
+})();
